530585546327279aebc67480cef5154807b3a8f2
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-01 01:14:31 +0700

    update ver3

--- js/game-dom.js
+++ js/game-dom.js
@@ -15,6 +15,7 @@
 //     alert(`Good Luck Have Fun ${inputNama}`);
 // }
 //isiGame
+
 function getPilihanComputer () {
     const comp = Math.random();
     if (comp <= 0.10) {
@@ -47,56 +48,121 @@ function getPilihanComputer () {
     return 'tornado';
 };
 
-function getHasil (comp, player) {
+function getHasil (comp, player, key) {
     if (player === comp){
-        return 'Nice Combo';
+        return `Nice Combo! ${key} = ${comp}`;
     } else {
         return 'Try Again';
     }
 };
 
+const pilComputer = getPilihanComputer();
+const pGambarSoal = document.querySelector('.img-computer');
+pGambarSoal.addEventListener('click', function(){
+    pGambarSoal.setAttribute('src', '/image/speels/'+pilComputer+'.png');
+    
+})
+
+const pilihan = document.querySelectorAll('li img');
 var comboPlayer = ''
-//quas
-const pQuas = document.querySelector('.quas');
-(pQuas.addEventListener('click', function(){
-    comboPlayer += 'q';
-    if (comboPlayer.length === 3) {
-        const pilComputer = getPilihanComputer();
-        const pilPlayer = comboPlayer;
-        const hasil = getHasil(pilComputer, pilPlayer);
-        comboPlayer = '';
-        console.log('comp :' + pilComputer);
-        console.log('player :' + pilPlayer);
-        console.log('hasil :' + hasil);
-    }
-}))
-//wex
-const pWex = document.querySelector('.wex');
-(pWex.addEventListener('click', function(){
-    comboPlayer += 'w';
-    if (comboPlayer.length === 3) {
-        const pilComputer = getPilihanComputer();
-        const pilPlayer = comboPlayer;
-        const hasil = getHasil(pilComputer, pilPlayer);
-        comboPlayer = '';
-        console.log('comp :' + pilComputer);
-        console.log('player :' + pilPlayer);
-        console.log('hasil :' + hasil);
-    }
-}))
-//exort
-const pExort = document.querySelector('.exort');
-(pExort.addEventListener('click', function(){
-    comboPlayer += 'e';
-    if (comboPlayer.length === 3) {
-        const pilComputer = getPilihanComputer();
-        const pilPlayer = comboPlayer;
-        const hasil = getHasil(pilComputer, pilPlayer);
-        comboPlayer = '';
-        console.log('comp :' + pilComputer);
-        console.log('player :' + pilPlayer);
-        console.log('hasil :' + hasil);
-    }
-}))
+pilihan.forEach(function(pil){
+     pil.addEventListener('click', function(){
+        const pilPlayer = pil.className;
+        if (pilPlayer === 'quas') {
+            comboPlayer += 'q';
+        }
+        if (pilPlayer === 'wex') {
+            comboPlayer += 'w';
+        }
+        if (pilPlayer === 'exort') {
+            comboPlayer += 'e';
+        }
+        if (comboPlayer.length === 3) {
+            var resultComboPlayer = comboPlayer;
+            var speelNow;
+            if (resultComboPlayer === 'qqq') {
+                speelNow = 'cold_snap';
+            } 
+            if (resultComboPlayer === 'www') {
+                speelNow = 'emp';
+            }
+            if (resultComboPlayer === 'eee') {
+                speelNow = 'sun_strike';
+            }
+            if (resultComboPlayer === 'wwe' || resultComboPlayer === 'wew' || resultComboPlayer === 'eww') {
+                speelNow = 'alacrity';
+            }
+            if (resultComboPlayer === 'eew' || resultComboPlayer === 'ewe' || resultComboPlayer === 'wee') {
+                speelNow = 'chaos_meteor';
+            }
+            if (resultComboPlayer === 'qwe' || resultComboPlayer === 'qew' || resultComboPlayer === 'wqe' || resultComboPlayer === 'weq' || resultComboPlayer === 'ewq' || resultComboPlayer === 'eqw') {
+                speelNow = 'deafening_blast';
+            }
+            if (resultComboPlayer === 'eeq' || resultComboPlayer === 'eqe' || resultComboPlayer === 'qee') {
+                speelNow = 'forge_spirit';
+            }
+            if (resultComboPlayer === 'qqw' || resultComboPlayer === 'qwq' || resultComboPlayer === 'wqq') {
+                speelNow = 'ghost_walk';
+            }
+            if (resultComboPlayer === 'qqe' || resultComboPlayer === 'qeq' || resultComboPlayer === 'eqq') {
+                speelNow = 'ice_wall';
+            }
+            if (resultComboPlayer === 'wwq' || resultComboPlayer === 'wqw' || resultComboPlayer === 'qww') {
+                speelNow = 'tornado';
+            }
+            const hasil = getHasil(pilComputer, speelNow, resultComboPlayer);
+            console.log('comp :' + pilComputer);
+            console.log('player :' + resultComboPlayer + ' ' + speelNow);
+            console.log('hasil :' + hasil);
+            const info = document.querySelector('.info');
+            info.innerHTML = hasil;
+            comboPlayer = '';
+        }
+     })
+})
+
+// var comboPlayer = ''
+// //quas
+// const pQuas = document.querySelector('.quas');
+// (pQuas.addEventListener('click', function(){
+//     comboPlayer += 'q';
+//     if (comboPlayer.length === 3) {
+//         const pilComputer = getPilihanComputer();
+//         const pilPlayer = comboPlayer;
+//         const hasil = getHasil(pilComputer, pilPlayer);
+//         comboPlayer = '';
+//         console.log('comp :' + pilComputer);
+//         console.log('player :' + pilPlayer);
+//         console.log('hasil :' + hasil);
+//     }
+// }))
+// //wex
+// const pWex = document.querySelector('.wex');
+// (pWex.addEventListener('click', function(){
+//     comboPlayer += 'w';
+//     if (comboPlayer.length === 3) {
+//         const pilComputer = getPilihanComputer();
+//         const pilPlayer = comboPlayer;
+//         const hasil = getHasil(pilComputer, pilPlayer);
+//         comboPlayer = '';
+//         console.log('comp :' + pilComputer);
+//         console.log('player :' + pilPlayer);
+//         console.log('hasil :' + hasil);
+//     }
+// }))
+// //exort
+// const pExort = document.querySelector('.exort');
+// (pExort.addEventListener('click', function(){
+//     comboPlayer += 'e';
+//     if (comboPlayer.length === 3) {
+//         const pilComputer = getPilihanComputer();
+//         const pilPlayer = comboPlayer;
+//         const hasil = getHasil(pilComputer, pilPlayer);
+//         comboPlayer = '';
+//         console.log('comp :' + pilComputer);
+//         console.log('player :' + pilPlayer);
+//         console.log('hasil :' + hasil);
+//     }
+// }))
 
         
